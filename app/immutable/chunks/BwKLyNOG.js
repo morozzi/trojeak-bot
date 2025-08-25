f9f9445b1510ce5d93bd85bd31e401c539337f20
“Morozzi” <shniager@gmail.com>
2025-08-26 02:02:20 +0700

    feat: miniapp source and build file

---
+++ app/immutable/chunks/BwKLyNOG.js
@@ -0,0 +1,1 @@
+import{h as a}from"./DrlzAb8W.js";a();
--- src/src/routes/+layout.svelte
+++ src/src/routes/+layout.svelte
@@ -1,17 +1,5 @@
 <script>
-	import { page } from '$app/stores';
-	import { goto } from '$app/navigation';
-	import { onMount } from 'svelte';
-	
 	import '../app.css';
-	
-	onMount(() => {
-		if ($page.url.pathname.includes('.php')) {
-			const queryString = $page.url.search;
-			console.log('Layout redirect - preserving query string:', queryString);
-			goto(`/${queryString}`, { replaceState: true });
-		}
-	});
 </script>
 
 <svelte:head>
