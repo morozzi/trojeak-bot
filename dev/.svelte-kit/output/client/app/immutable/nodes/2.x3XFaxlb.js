5cd0d0811a1f6bb5d0bf9802bbb078f7a735bc6c
“Morozzi” <shniager@gmail.com>
2025-09-18 04:43:13 +0700

    Bot Update

--- dev/.svelte-kit/output/client/app/immutable/nodes/2.x3XFaxlb.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"../chunks/BhSRmIAj.js";export{m as component};
--- dev/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["app/immutable/nodes/1.C3GziQJN.js","app/immutable/chunks/Bzak7iHL.js","app/immutable/chunks/Cw07epn-.js","app/immutable/chunks/DSYlFEXo.js","app/immutable/chunks/DW4CAaEY.js","app/immutable/chunks/CP9mP3Eo.js","app/immutable/chunks/BfTNCZCY.js"];
-export const stylesheets = [];
-export const fonts = [];
