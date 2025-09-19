37027e4f07e09a4086ac5563366a59bfac0b45c1
“Morozzi” <shniager@gmail.com>
2025-09-20 03:08:11 +0700

    Miniapp template update

---
+++ app/immutable/entry/start.BW-8JAWe.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Ck02Fg9m.js";export{o as load_css,r as start};
--- app/immutable/entry/start.Dt5cRwTo.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Yl7_qcbM.js";export{o as load_css,r as start};
