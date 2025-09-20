d4dfe6231692a5e4786842fd4d9d845fc3594ff3
“Morozzi” <shniager@gmail.com>
2025-09-20 07:39:35 +0700

    Miniapp template update

--- app/immutable/nodes/2.DGTe1BTN.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"../chunks/BqCaiaK_.js";export{m as component};
--- dev/src/routes/+page.svelte
+++ dev/src/routes/+page.svelte
@@ -187,7 +187,7 @@
 				/>
 			{/if}
 
-			<main class="mx-auto w-full max-w-2xl px-4 pt-0">
+			<main class="mx-auto w-full max-w-2xl px-4 pt-0 mb-8">
 				{#if $appStore.currentView === 'home'}
 					<Home 
 						on:goToEvent={handleGoToEvent}
